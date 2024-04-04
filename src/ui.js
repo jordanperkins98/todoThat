40b6cc296eb0c68e07de63481d3305304774eef1
Jordan Perkins <jordanperkins@hotmail.co.uk>
2024-04-04 20:48:01 +0100

    Add icons to sidebar

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,9 @@
 import Todo from './todo.js';
 import Project from './todoProject.js';
 import './styles.css';
+import bellIcon from './Assets/bell-outline.png';
+import sidebarIcon from './Assets/sidebar.png';
+import addTaskIcon from './Assets/plus-circle-custom.png';
 
 
 const newTodo = Todo.new('Take the bins out','Take the bins out for Mum & Dad','Normal',null,null)
@@ -8,3 +11,25 @@ const newTodo = Todo.new('Take the bins out','Take the bins out for Mum & Dad','
 newTodo.addLabel('Chores')
 
 console.log(newTodo)
+
+function iconToDiv(imgPath,divNameForicon){
+    const element = document.createElement('div')
+    element.classList.add('iconDiv')
+    const icon = new Image();
+    icon.src = imgPath
+    icon.classList.add(divNameForicon);
+    element.appendChild(icon)
+    return element
+}
+
+document.querySelector('.icons').appendChild(iconToDiv(bellIcon,'notificationsIcon'));
+document.querySelector('.icons').appendChild(iconToDiv(sidebarIcon,'siderbarIcon'));
+
+
+const button = document.querySelector('.newTodo')
+
+const addIcon = new Image();
+addIcon.src = addTaskIcon;
+addIcon.classList.add('addTaskIcon')
+button.prepend(addIcon);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { type } = require('os');
 
 module.exports = {
     mode: 'development',
@@ -24,6 +25,10 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
         ],
     },
