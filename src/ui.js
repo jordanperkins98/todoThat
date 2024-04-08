cfa4f5e0278ed528323056b5b43ba8bce5f3d9a8
Jordan Perkins <jordanperkins@hotmail.co.uk>
2024-04-08 20:58:18 +0100

    Update sidebar styling and functionality

--- src/ui.js
+++ src/ui.js
@@ -12,24 +12,40 @@ newTodo.addLabel('Chores')
 
 console.log(newTodo)
 
-function iconToDiv(imgPath,divNameForicon){
-    const element = document.createElement('div')
-    element.classList.add('iconDiv')
-    const icon = new Image();
-    icon.src = imgPath
-    icon.classList.add(divNameForicon);
-    element.appendChild(icon)
-    return element
+sidebar();
+
+function sidebar() {
+    function iconToDiv(imgPath, divNameForicon) {
+        const element = document.createElement('div');
+        element.classList.add('iconDiv');
+        const icon = new Image();
+        icon.src = imgPath;
+        icon.classList.add(divNameForicon);
+        element.appendChild(icon);
+        return element;
+    }
+
+    document.querySelector('.icons').appendChild(iconToDiv(bellIcon, 'notificationsIcon'));
+    const toggleButton = iconToDiv(sidebarIcon, 'siderbarIcon');
+    toggleButton.classList.add('toggleButton');
+
+
+
+
+    document.querySelector('.icons').appendChild(toggleButton);
+
+
+    const button = document.querySelector('.newTodo');
+
+    const addIcon = new Image();
+    addIcon.src = addTaskIcon;
+    addIcon.classList.add('addTaskIcon');
+    button.prepend(addIcon);
 }
 
-document.querySelector('.icons').appendChild(iconToDiv(bellIcon,'notificationsIcon'));
-document.querySelector('.icons').appendChild(iconToDiv(sidebarIcon,'siderbarIcon'));
-
-
-const button = document.querySelector('.newTodo')
-
-const addIcon = new Image();
-addIcon.src = addTaskIcon;
-addIcon.classList.add('addTaskIcon')
-button.prepend(addIcon);
-
+document.querySelector('.toggleButton').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.toggle('hide');
+    document.querySelector('.toggleButton').classList.toggle('show');
+ 
+  });
+  
